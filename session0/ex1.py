3d3bbe0fee542b1b29805a9e3a58ac733112fc74
gialinh <linh.transcend@hcmut.edu.vn>
2024-06-23 19:39:09 +0700

    Create ex1.py

---
+++ session0/ex1.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age: "))
+dream_job = input("Enter your dream job: ")
+prefered_weapon = input("Enter your prefered weapon: ")
+
+print(f"I am {age} years old. My dream job is {dream_job}. I will use {prefered_weapon} to achieve my dream job.")
